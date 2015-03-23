981b7b5daaeeebbe8fd761dde309e73f7aa43e96
walid <walid.sa3d@gmail.com>
2015-03-23 15:00:06 +0100

    use envelopes module

--- postman.py
+++ postman.py
@@ -1,11 +1,10 @@
 import csv
 from jinja2 import Template
 from jinja2 import Environment, meta
-import smtplib
-from email.mime.multipart import MIMEMultipart
-from email.mime.text import MIMEText
 import argparse
 import json
+from envelopes import Envelope
+
 class bostman:
 
     def getTemplate(self,template_name):
@@ -27,26 +26,26 @@ class bostman:
     def parse_config(self):
         with open('config.json') as data_file:    
             data = json.load(data_file)
-        return data
+        self.server=data['server']
+        self.port=data['port']
+        self.login=data['login']
+        self.password=data['password']
+        
     def send_mail(self,name,to,subject,message):
-        msg = MIMEMultipart('alternative')
-        msg['subject']=subject
-        msg['to']=to
-        msg['from']=self.parse_config()['login']
-        part1 = MIMEText('nothing', 'plain')
-        part2 = MIMEText(message, 'html')
-        msg.attach(part1)
-        msg.attach(part2)
-        server = smtplib.SMTP(self.parse_config()['server']+":"+self.parse_config()['port'])
-        server.starttls()
-        server.login(self.parse_config()['login'],self.parse_config()['password'])
-        server.sendmail(name, to, msg.as_string())
-        server.quit()
+        self.parse_config()
+        envelope = Envelope(
+        from_addr=(self.parse_config()['login']),
+        to_addr=(to),
+        subject=subject,
+        text_body=message
+            )
+        envelope.send(self.server, login=self.login,
+              password=self.password, tls=True)
     def main(self):
         parser = argparse.ArgumentParser(usage="-h for full usage")
         parser.add_argument('-t', dest="template", help='email template',required=True)
         parser.add_argument('-c', dest="csv", help='list of emails',required=True)
-        parser.add_argument('-subject', dest="subject", help='email subject',required=True)
+        parser.add_argument('-s', dest="subject", help='email subject',required=True)
         args = parser.parse_args()
         template=self.getTemplate(args.template)
         for line in self.read_mail_list(args.csv):
